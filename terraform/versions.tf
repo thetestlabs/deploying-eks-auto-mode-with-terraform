124d61c876a5874676745b11f1be2a04ea82dffe
Chris McQuaid <chris@thetestlabs.io>
2024-12-31 16:37:12 +0000

    fix: update minimum provider versions based on eks module

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,11 +1,10 @@
 terraform {
-  required_version = "~> 1.10.0"
+  required_version = ">= 1.3.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 5.81"
     }
-
   }
 }
